5deb81684a67a86f9a67fd5158df8e3fd0d3c748
KishanRathod17 <97015515+KishanRathod17@users.noreply.github.com>
2023-08-02 21:05:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:cldc/firebase_options.dart';
 import 'package:cldc/screens/login_cldc.dart';
 import 'package:cldc/screens/task_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 // import 'package:cldc/screens/task_screen.dart'
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
